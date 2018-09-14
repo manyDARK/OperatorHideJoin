8dcf040b3c563ffc5c7021a714a6a8b88745d9b1
Crypt97 <crypthu@gmail.com>
2018-09-14 16:10:26 +0200

    Add files via upload

---
+++ src/Angel/OperatorHideJoin/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Angel\OperatorHideJoin;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+       $this->getServer()->getPluginManager()->registerEvents($this, $this);
+       $this->saveDefaultConfig();
+	   $this->getLogger()->info(TextFormat::DARK_GREEN . "Operator join is invisible now." . TextFormat::GREEN . " (Crypt)");
+    }
+	
+	public function onJoin(PlayerJoinEvent $event){
+		if($event->getPlayer()->isOp()){
+		   $player = $event->getPlayer();
+		   $name = $player->getName();
+		   $event->setJoinMessage(false);
+		}
+	}
+	
+	public function onQuit(PlayerQuitEvent $event){
+		if($event->getPlayer()->isOp()){
+		   $player = $event->getPlayer();
+		   $name = $player->getName();
+		   $event->setQuitMessage(false);
+		}
+	}
+}
